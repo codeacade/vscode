c5695e17ef11240bc3e538aca8cad1a630f5b007
code <33802433+codeacade@users.noreply.github.com>
2021-05-13 00:30:34 +0100

    image promise added

--- sss.js
+++ sss.js
@@ -6,3 +6,20 @@ document.body.appendChild(myImage);
 
 //  equivalent of <img width="100" height="200" src="picture.jpg">
 
+/////////////////////////////////////////////////////////////////
+//  Example of async image loading with Promises:
+
+function asyncImg(url) {
+  return new Promise((resolve, reject) => {    
+    const img = new Image()   //  Image() is build-in DOM object
+    resolve(img)              //  assign img object to succesfull Promise (resolve)
+    img.src = url             //  assign url parameter as image cource
+  });
+}
+
+
+asyncImg('cat1.jpg').then(                   //  call Promise function and call .then
+    img => document.body.appendChild(img),   //  option for succesfull Promise, and
+    console.log("ALERT")                     //  option for unsuccesfull Promise
+)
+
